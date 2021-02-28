222ce6c82eb0ca80a1389262ea6be6bf01d823fe
DESKTOP-U7ONH0K\Aqib <aqibbutt3078@gmail.com>
2021-02-28 18:27:38 +0100

    lecture 1

---
+++ Evslec1/src/MainClass.java
@@ -0,0 +1,55 @@
+//import java.util.Scanner;
+
+/// #include <iostream.h>
+public class MainClass {
+
+	public static void main(String[] args) {
+		int x = 55;
+
+//		if (x != 55) {
+//			System.out.println("Hi");
+//		} else if (x == 55) {
+//			System.out.println("HI 1");
+//		} else if (x <= 55) {
+//			System.out.println("H1 2");
+//		} else {
+//			System.out.println("Bye");
+//		}
+
+//		if (x != 55) {
+//			System.out.println("Hi");
+//		} else {
+//			System.out.println("Bye");
+//		}
+
+//		Boolean boole = true;
+//		Integer integer = 1;
+//		boolean bool = true;
+//		double dd = 11.11;
+//		float f = 12.3f;
+//		long y = 123L;
+//		int v = 111;
+//		short s = 123;
+//		System.out.println(s);
+//		byte b = 17;
+//		byte b1 = 13;
+//		byte add = (byte)(b + b1);
+//		System.out.println(b);
+
+		// Preemptive types
+		// byte = 8 bits
+		// short = 16 bits
+		// int = 32 bits
+		// long = 64 bits
+		// float = 32 bits
+		// double = 64 bits
+		// boolean = 1 bit
+
+		// object ?
+//		Scanner in = new Scanner(System.in);
+//		System.out.print("Enter your name: ");
+//		String name = in.next();
+//		System.out.println("Hi !! "+ name);
+//		in.close();
+	}
+}
